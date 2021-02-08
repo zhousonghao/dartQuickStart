2c83fa5a5ac8ccad0621dadb6bc974c4bcdb55c6
songhao zhou <songhaozhou@songhaos-mbp-2.myfiosgateway.com>
2021-02-08 01:05:35 -0500

    end of step 3

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,12 +2,13 @@ import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
+import 'src/hero_component.dart';
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [coreDirectives, formDirectives],
+  directives: [coreDirectives, formDirectives, HeroComponent],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
